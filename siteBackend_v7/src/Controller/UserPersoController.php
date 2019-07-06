9e4759b4b193009a7b7b2a0065a7947bbae81f04
« traore » <traoreerick@gmail.com>
2019-07-06 12:59:54 +0000

    test ok avec vue du formulaire userPerso, mais attention, pas de persistence en bdd

--- siteBackend_v7/src/Controller/UserPersoController.php
+++ siteBackend_v7/src/Controller/UserPersoController.php
@@ -2,18 +2,25 @@
 
 namespace App\Controller;
 
+
+use App\Entity\UserPerso;
+use App\Form\UserPersoType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserPersoController extends AbstractController
 {
     /**
-     * @Route("/user/perso", name="user_perso")
+     * @Route("/perso", name="perso")   
      */
     public function index()
     {
+        $userPerso = new UserPerso();
+        
+        $form = $this->createForm(UserPersoType::class, $userPerso);
+        
         return $this->render('user_perso/index.html.twig', [
-            'controller_name' => 'UserPersoController',
+            'form' => $form->createView()
         ]);
     }
 }
